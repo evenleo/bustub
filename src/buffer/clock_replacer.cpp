e307bafa46b9e3baf990eefcbf4a0f9c27fd57b7
evenleo <1293173298@qq.com>
2022-02-21 22:05:52 +0800

    添加注释

--- src/buffer/clock_replacer.cpp
+++ src/buffer/clock_replacer.cpp
@@ -18,9 +18,10 @@ ClockReplacer::ClockReplacer(size_t num_pages) : num_pages_(num_pages) {}
 
 ClockReplacer::~ClockReplacer() = default;
 
+// Victim(受害者)，可理解为牺牲
 bool ClockReplacer::Victim(frame_id_t *frame_id) { 
   std::lock_guard<std::mutex> guard(latch_);
-  if (lru_.empty()) {
+  if (lru_.empty()) {  // 没有可以牺牲的页直接返回false
     return false;
   } 
   *frame_id = lru_.back();
@@ -29,6 +30,8 @@ bool ClockReplacer::Victim(frame_id_t *frame_id) {
   return true; 
 }
 
+// pin函数表示这个frame被某个进程引用了
+// 被引用的frame不能成为LRU算法的牺牲目标，所以这里把它从我们的数据结构中删除
 void ClockReplacer::Pin(frame_id_t frame_id) {
   std::lock_guard<std::mutex> guard(latch_);
   auto it = table_.find(frame_id);
@@ -38,6 +41,9 @@ void ClockReplacer::Pin(frame_id_t frame_id) {
   } 
 }
 
+// 先看一下这个页是否在可替换链表中
+// 如果它不存在的话。则需要看一下当前链表是否还有空闲位置。如果有的话则直接加入
+// 如果没有则需要移除链表尾部的节点知道有空余位置
 void ClockReplacer::Unpin(frame_id_t frame_id) {
   std::lock_guard<std::mutex> guard(latch_);
   auto it = table_.find(frame_id);
